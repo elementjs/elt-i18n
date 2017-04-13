b41361aa150a16155e615de829d59cf23794923e
Christophe Eymard <christophe.eymard@sales-way.com>
2017-04-13 17:19:57 +0200

    use Node | string for translation.

--- i18n.ts
+++ i18n.ts
@@ -21,11 +21,14 @@ export function P(n: number, plural: string, singular: string) {
 
 export function S(n: number) { return P(n, 's', '')}
 
-export function L(l: Languages<string>): I18nObservable
+export function L(l: Languages<Node>): I18nObservable<Node>
+export function L(l: Languages<string>): I18nObservable<string>
+export function L(l: Languages<string|Node>): I18nObservable<string|Node>
 export function L<T>(l: Languages<TradFn<T>>): I18nObject<T>
 export function L(l: Languages<any>): any {
   for (var x in l) {
-    if (typeof (l as any)[x] === 'string') {
+    var prop = (l as any)[x]
+    if (typeof prop === 'string' || prop instanceof Node) {
       var res = new I18nObservable()
       res.trads = l as any // I know what I'm doing !!
       return res
@@ -38,14 +41,14 @@ export function L(l: Languages<any>): any {
 }
 
 
-export class I18nObservable extends TransformObservable<string, string> {
-  public trads: {[code: string]: string} = {}
+export class I18nObservable<T> extends TransformObservable<string, T> {
+  public trads: {[code: string]: string|Node} = {}
 
   constructor() {
     super(
       LANG,
-      function (this: I18nObservable, lang: string) {
-        if (!this.trads || !this.trads[lang]) return ''
+      function (this: I18nObservable<T>, lang: string) {
+        if (!this.trads || !this.trads[lang]) return '' as any
         return this.trads[lang]
       }, undefined, undefined)
   }
